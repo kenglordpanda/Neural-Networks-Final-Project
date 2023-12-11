544acf33fa9b2db433fd9d317aad26202c9f6972
kenglordpanda <ktran7246@gmail.com>
2023-12-10 21:34:07 -0600

    Got it to compile

--- checkwork.py
+++ checkwork.py
@@ -1,17 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-"""
-Research paper does not show that bias is implemented so it is not included in this code
-"""
-
 
 class Perceptron:
     def __init__(self, num_inputs, learning_rate):
         self.weights = np.random.uniform(-0.1, 0.1, (num_inputs, 1))
         self.bias = np.random.rand()
         self.learning_rate = learning_rate
-        self.prev_responses = []
 
     def sigmoid(self, input):
         return 1 / (1 + np.exp(-input))
@@ -21,30 +16,12 @@ class Perceptron:
         return self.sigmoid(output_layer_in)
 
     def update_weights(self, inputs, target):
-        delta_weights = np.zeros(self.weights.size)
+        delta_weights = np.zeros(self.weights.shape)
         network_response = self.compute_response(inputs)
         generated_response = np.random.rand()
         if generated_response <= network_response:
-            delta_weights = self.learning_rate * target * inputs
+            delta_weights = (self.learning_rate * target) * inputs
         self.weights += delta_weights
-
-
-def generate_specific_epochs(
-    num_stimuli, epoch_start, epoch_end, reinforcement_probabilities
-):
-    if num_stimuli != len(reinforcement_probabilities):
-        return None
-    range = epoch_end - epoch_start
-    list_sizes = np.floor(np.array(reinforcement_probabilities) * range).astype(int)
-    stimuli_reinforcements = [
-        set(
-            np.random.choice(
-                np.arange(epoch_start, epoch_end + 1), size=size, replace=False
-            )
-        )
-        for size in list_sizes
-    ]
-    return stimuli_reinforcements
 
 
 def modify_reinforcement(current_reinforcement):
@@ -53,21 +30,47 @@ def modify_reinforcement(current_reinforcement):
     return modified_rates
 
 
+def average_response(perceptrons, presentations):
+    averages = []
+    for presentation in presentations:
+        sum = 0
+        for perceptron in perceptrons:
+            sum += perceptron.compute_response(presentation)
+        averages.append(sum)
+    return averages
+
+
 def simulate_bandit(
     num_inputs, num_networks, num_epochs, init_reinforcement, learning_rate
 ):
+    stimuli_average_response = [[] for _ in range(num_inputs)]
     reinforcement_rates = init_reinforcement
     perceptrons = []
-    presenations = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
-
+    presentations = []
+    for init_val in range(num_inputs):
+        stimulus = []
+        for index in range(num_inputs):
+            if index == init_val:
+                stimulus.append([1])
+            else:
+                stimulus.append([0])
+        presentation = np.array(stimulus)
+        presentations.append(presentation)
     for _ in range(num_networks):
         perceptrons.append(Perceptron(num_inputs, learning_rate))
 
     for epoch in range(num_epochs):
         if epoch >= num_epochs // 2:
             reinforcement_rates = modify_reinforcement(reinforcement_rates)
-
-    return
+        for index, presentation in enumerate(presentations):
+            desired_value = 0 if np.random.rand() > reinforcement_rates[index] else 1
+            for perceptron in perceptrons:
+                perceptron.update_weights(presentation, desired_value)
+        if epoch % 20 == 0:
+            averages = average_response(perceptrons, presentations)
+            for i, average in enumerate(averages):
+                stimuli_average_response[i].append(average)
+    return stimuli_average_response
 
 
 def main():
@@ -76,13 +79,10 @@ def main():
     num_epochs = 600
     reinforcement = [0.2, 0.4, 0.6, 0.8]
     learning_rate = 0.1
-    simulate_bandit(
+    values = simulate_bandit(
         num_stimuli, num_perceptrons, num_epochs, reinforcement, learning_rate
     )
-    example_reinforcement = generate_specific_epochs(4, 0, 300, reinforcement)
-    for i, s in enumerate(example_reinforcement):
-        print(f"Set {i + 1}: {sorted(s)}")
-    return 0
+    print(f"Bandit Simulated")
 
 
 if __name__ == "__main__":
