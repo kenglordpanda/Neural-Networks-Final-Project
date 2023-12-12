d7adcc14ec9489493612f841c823039fc15780de
kenglordpanda <ktran7246@gmail.com>
2023-12-11 18:59:05 -0600

    Got everything working

--- checkwork.py
+++ checkwork.py
@@ -19,8 +19,9 @@ class Perceptron:
         delta_weights = np.zeros(self.weights.shape)
         network_response = self.compute_response(inputs)
         generated_response = np.random.rand()
+        error = target - network_response
         if generated_response <= network_response:
-            delta_weights = (self.learning_rate * target) * inputs
+            delta_weights = (self.learning_rate * error) * inputs
         self.weights += delta_weights
 
 
@@ -36,7 +37,7 @@ def average_response(perceptrons, presentations):
         sum = 0
         for perceptron in perceptrons:
             sum += perceptron.compute_response(presentation)
-        averages.append(sum)
+        averages.append(sum.item() / len(perceptrons))
     return averages
 
 
@@ -59,11 +60,18 @@ def simulate_bandit(
     for _ in range(num_networks):
         perceptrons.append(Perceptron(num_inputs, learning_rate))
 
-    for epoch in range(num_epochs):
+    # get initialze average response
+    init_avgs = average_response(perceptrons, presentations)
+    for i, average in enumerate(init_avgs):
+        stimuli_average_response[i].append(average)
+
+    for epoch in range(1, num_epochs + 1):
         if epoch >= num_epochs // 2:
             reinforcement_rates = modify_reinforcement(reinforcement_rates)
         for index, presentation in enumerate(presentations):
-            desired_value = 0 if np.random.rand() > reinforcement_rates[index] else 1
+            desired_value = 1
+            if np.random.rand() > reinforcement_rates[index]:
+                desired_value = 0
             for perceptron in perceptrons:
                 perceptron.update_weights(presentation, desired_value)
         if epoch % 20 == 0:
@@ -82,7 +90,21 @@ def main():
     values = simulate_bandit(
         num_stimuli, num_perceptrons, num_epochs, reinforcement, learning_rate
     )
-    print(f"Bandit Simulated")
+    print(values[0])
+    epoch_recorded_values = [20 * x for x in range(0, num_epochs // 20 + 1)]
+
+    plt.plot(epoch_recorded_values, values[0], color="r", label="DS1")
+    plt.plot(epoch_recorded_values, values[1], color="b", label="DS2")
+    plt.plot(epoch_recorded_values, values[2], color="g", label="DS3")
+    plt.plot(epoch_recorded_values, values[3], color="y", label="DS4")
+
+    plt.xlabel("Epoch of Training")
+    plt.ylabel("Mean Network Activation")
+    plt.legend()
+
+    plt.title("Average Network Response to Discriminative Stimuli")
+    plt.show()
+    plt.savefig("graph.png")
 
 
 if __name__ == "__main__":
